606eb03a337c18f3ba85d05c535caf245f8473f1
cece0719 <cece0719@naver.com>
2015-12-04 14:02:45 +0900

    asdf

--- src/main/java/com/woowol/gutenmorgen/GutenmorgenApplication.java
+++ src/main/java/com/woowol/gutenmorgen/GutenmorgenApplication.java
@@ -1,28 +1,22 @@
 package com.woowol.gutenmorgen;
 
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
-import java.io.IOException;
-
 @SpringBootApplication
+@PropertySource({
+        "classpath:properties/application.properties",
+        "classpath:properties/application-${spring.profiles.active}.properties"
+})
 public class GutenmorgenApplication {
     public static ConfigurableApplicationContext ctx;
+
     public static void main(String[] args) {
         ctx = SpringApplication.run(GutenmorgenApplication.class, args);
-    }
-
-    @Bean
-    public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() throws IOException {
-        return new PropertyPlaceholderConfigurer() {{
-            setLocations((new PathMatchingResourcePatternResolver()).getResources("classpath:/properties/**/*.properties"));
-        }};
     }
 
     @Bean
