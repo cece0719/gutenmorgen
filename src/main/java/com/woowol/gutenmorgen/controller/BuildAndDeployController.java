2211bd8884a79089859f1787999086796fcfac21
cece0719 <cece0719@naver.com>
2015-12-07 20:01:24 +0900

    sdfasd

--- src/main/java/com/woowol/gutenmorgen/controller/BuildAndDeployController.java
+++ src/main/java/com/woowol/gutenmorgen/controller/BuildAndDeployController.java
@@ -1,6 +1,5 @@
 package com.woowol.gutenmorgen.controller;
 
-import com.woowol.gutenmorgen.GutenmorgenApplication;
 import com.woowol.gutenmorgen.model.Result;
 import com.woowol.gutenmorgen.model.Result.ReturnCode;
 import com.woowol.gutenmorgen.util.Validate;
@@ -10,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Slf4j
@@ -26,14 +24,6 @@ public class BuildAndDeployController {
     @ResponseBody
     public Result healthCheck() {
         return new Result(ReturnCode.SUCCESS, status);
-    }
-
-    @RequestMapping("/shutdown")
-    @ResponseBody
-    public String shutdown(HttpServletRequest request) {
-        GutenmorgenApplication.ctx.close();
-        log.info(request.getRemoteAddr());
-        return "OK";
     }
 
     @RequestMapping("/go.json")
